68689ed1ad2782d82b514a58c0d522dadbbdeaa8
Olivier_P <43119712+silentswordxv@users.noreply.github.com>
2023-04-10 00:12:10 -0400

    Implemented Display/Edit/Delete/Create Infection (HasCaught) in the manager

--- main/Infection/displayInfection.php
+++ main/Infection/displayInfection.php
@@ -1,6 +1,8 @@
 <?php require_once '../../database.php';
 if (isset($conn)) {
-    $statement = $conn->prepare('SELECT * FROM '.DBNAME.'.Infection');
+    $statement = $conn->prepare('SELECT * FROM '.DBNAME.'.HasCaught HC
+        JOIN '.DBNAME.'.Infection I
+        ON HC.infectionID = I.infectionID ORDER BY employeeID');
     $statement->execute(); //executes the query above
 } else {
     print_r("PDO CONN ERROR");
@@ -17,44 +19,50 @@ if (isset($conn)) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Infection Table</title>
-    <link rel ="stylesheet" href="../../css/infection.css">
+    <link rel ="stylesheet" href="../../css/displayTable.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
 </head>
 <body>
 
-  <div id = "page-container">
-          <div id = "page-wrap">
-              <?php include '../navBar.php';?>
-              <?php include '../searchBar.php';?>
-              
-              <h1 style='text-align:center; font-family:Museosans,serif; margin-top:10px'> Infections </h1>
-              <div class="table-condensed">
-              <table class="table" style= "padding:20px;">
-                <thead>
-                  <tr class="hoverUpon">
-                    <th scope="col" style="font-size:10px">Infection ID</th>
-                    <th scope="col" style="font-size:10px">Type of Infection</th>
+<div id = "page-container">
+    <div id = "page-wrap">
+        <?php include '../navBar.php';?>
+        <?php include '../searchBar.php';?>
 
-                  </tr>
+        <h1 style='text-align:center; font-family:Museosans,serif; margin-top:10px'> List of Infection Cases </h1>
+        <div class="table-condensed">
+            <table class="table" style= "padding:20px;">
+                <thead>
+                <tr class="hoverUpon">
+                    <th scope="col" style="font-size:10px">Date of Infection</th>
+                    <th scope="col" style="font-size:10px">Employee ID</th>
+                    <th scope="col" style="font-size:10px">Type of Infection</th>
+                    <th scope="col" style="font-size:10px">Infection Case ID</th>
+                    <th scope="col" style="font-size:10px">Options</th>
+
+                </tr>
                 </thead>
                 <tbody>
-                  <?php while ($row = $statement->fetch(PDO::FETCH_ASSOC, PDO::FETCH_ORI_NEXT)) { ?>
+                <?php while ($row = $statement->fetch(PDO::FETCH_ASSOC, PDO::FETCH_ORI_NEXT)) { ?>
                     <tr class="hoverUpon">
-                      <td tyle="font-size:10px"><?= $row["infectionID"] ?></td>
-                      <td style="font-size:10px"><?= $row["typeOfInfection"] ?></td>
+                        <td style="font-size:15px"><?= $row["dateOfInfection"] ?></td>
+                        <td style="font-size:15px"><?= $row["employeeID"] ?></td>
+                        <td style="font-size:15px"><?= $row["typeOfInfection"] ?></td>
+                        <td style="font-size:15px"><?= $row["infectionCaseID"] ?></td>
 
-                      <td>
-                        <a style="font-size:10px" href="deleteEmployee.php?ID=<?= $row["infectionID"] ?>"> Delete </a>
-                        <a style="font-size:10px" href="editEmployee.php?ID=<?= $row["infectionID"] ?>"> Edit </a>
-                      </td>
-                  </tr>
-                  <?php } ?>
+                        <td>
+                            <a style="font-size:15px" href="deleteInfection.php?ID=<?= $row["infectionCaseID"] ?>"> Delete </a>
+                            <a style="font-size:15px" href="editInfection.php?ID=<?= $row["infectionCaseID"] ?>"> Edit </a>
+                        </td>
+                    </tr>
+                <?php } ?>
                 </tbody>
-              </table>
-              <div>
-              <?php include '../footer.php';?>
-          <div>
-      <div>
+            </table>
+            <div>
+                <?php include '../footer.php';?>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
--- main/Infection/insertIntoInfection.php
+++ main/Infection/insertIntoInfection.php
@@ -2,31 +2,35 @@
 require_once '../../database.php';
 $success = false;
 if (isset($conn)) {
-    $maxIDFetch = $conn->prepare('SELECT max(' . DBNAME . '.Infection.infectionID) FROM ' . DBNAME . '.Infection');
-    $maxIDFetch->execute();
-    $maxInfectionID = $maxIDFetch->fetchColumn();
-    $id = $maxInfectionID + 1;
-
+    $optionFetch = $conn->prepare('SELECT * FROM '.DBNAME.'.Infection order by infectionID');
+    $optionFetch->execute();
+    $options = $optionFetch->fetchAll();
 
     if (isset($_POST['submit'])) {
-        $typeOfInfection = $_POST['lastName'];
+        $dateOfInfection = $_POST['dateOfInfection'];
+        $employeeID = $_POST['employeeID'];
+        $infectionID = $_POST['infectionID'];
+        $infectionCaseID = $_POST['infectionCaseID'];
+
+        $infectionID = intval($infectionID);
 
 
         // prepare the statement
-        $sql = "INSERT INTO " . DBNAME . ".Infection (infectionID, typeOfInfection) VALUES (:infectionID, :typeOfInfection)";
+        $sql = "INSERT INTO " . DBNAME . ".HasCaught (dateOfInfection, employeeID, infectionID, infectionCaseID)
+            VALUES (:dateOfInfection, :employeeID, :infectionID, :infectionCaseID)";
         $stmt = $conn->prepare($sql);
 
         // bind the parameters
-        $stmt->bindParam(':id', $id);
-        $stmt->bindParam(':typeOfInfection', $typeOfInfection);
+        $stmt->bindParam(':dateOfInfection', $dateOfInfection);
+        $stmt->bindParam(':employeeID', $employeeID);
+        $stmt->bindParam(':infectionID', $infectionID);
+        $stmt->bindParam(':infectionCaseID', $infectionCaseID);
 
 
         // execute the statement
         if ($stmt->execute()) {
-            // echo "Entries added";
             $success = true;
         } else {
-            // echo "Error: " . $stmt->errorInfo()[2];
             $success = false;
         }
     }
@@ -45,82 +49,64 @@ if (isset($conn)) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>DisplayEmployeesTable</title>
-    <link rel ="stylesheet" href="displayTable.css">
+    <title>Log A New Infection Case</title>
+    <link rel ="stylesheet" href="../../css/displayTable.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
 </head>
 <body>
 
-  <div id = "page-container">
-          <div id = "page-wrap" style="width:100%">
-              <?php include_once '../navBar.php';?>
-              <?php include_once '../searchBar.php';?>
-              
-              <h1 style='text-align:center; font-family:Museosans; margin-top:10px'> Insert an employee record </h1>
-              <div id = "insertEmployeeForm" style="margin-top:10px">
+<div id = "page-container">
+    <div id = "page-wrap" style="width:100%">
+        <?php include_once '../navBar.php';?>
+        <?php include_once '../searchBar.php';?>
 
-              <form style="width:100%; padding:30px" method="POST" >
+        <h1 style='text-align:center; font-family:Museosans,serif;serif margin-top:10px'>Log A New Infection Case</h1>
+        <div id = "insertInfectionForm" style="margin-top:10px">
+            <form style="width:100%; padding:30px" method="POST" >
                 <div class="form-row">
-                    <div class="form-group col-md-5">
-                        <label for="firstName">First Name</label>
-                        <input type="text" class="form-control" id="firstName" name = "firstName" placeholder="FirstName" required>
+                    <div class="form-group col-md-3">
+                        <label for="typeOfInfection">Employee ID</label>
+                        <label for="employeeID"></label><input type="number" min="0" class="form-control" id="employeeID" name = "employeeID" placeholder="employeeID" required>
                     </div>
-                    <div class="form-group col-md-5">
-                        <label for="lastName">Last Name</label>
-                        <input type="text" class="form-control" id="lastName" name = "lastName" placeholder="LastName" required>
+                    <div class="form-group col-md-3">
+                        <label for="infectionCaseID">Infection Case ID</label>
+                        <input type="text" class="form-control" id="infectionCaseID" name = "infectionCaseID" placeholder="infectionCaseID" required>
                     </div>
-                    <div class="form-group col-md-2">
-                        <label for="dateOfBirth">Date Of Birth (YYYY-MM-DD)</label>
-                        <input type="text" class="form-control" id="dateOfBirth" name = "dateOfBirth" placeholder="YYYY-MM-DD" required>
-                    </div>
-                </div>
-                <div class="form-row">
                     <div class="form-group col-md-4">
-                        <label for="citizenship">Citizenship</label>
-                        <input type="text" class="form-control" id="citizenship" name = "citizenship" placeholder="Citizenship" required>
-                    </div>
-                    <div class="form-group col-md-6">
-                        <label for="email">Email Address</label>
-                        <input type="email" class="form-control" id="email" name = "email" placeholder="Email Address" required>
-                    </div>
-                    <div class="form-group col-md-2">
-                        <label for="country">Country</label>
-                        <input type="text" class="form-control" id="country" name = "country" value = "Canada" disabled>
-                    </div>
-                </div>
-                <div class="form-row">
-                    <div class="form-group col-md-2">
-                    <label for="province">Province</label>
-                    <input type="text" class="form-control" id="province" name = "province" placeholder = "Province" required>
+                        <label for="infectionID">Type of Infection</label>
+                        <select class="form-select" aria-label="selectTypeOfInfection" id="infectionID" name = "infectionID" required>
+                            <?php
+                            if (isset($options)) {
+                                foreach ($options as $option) {
+                                    echo "<option value=" . $option['infectionID'] . ">" . $option['typeOfInfection'] . "</option>";
+                                }
+                            }
+                            ?>
+                        </select>
                     </div>
                     <div class="form-group col-md-2">
-                    <label for="city">City</label>
-                    <input type="text" class="form-control" id="city" name = "city" placeholder = "City" required>
-                    </div>                    
-                    <div class="form-group col-md-6">
-                    <label for="address">Address</label>
-                    <input type="text" class="form-control" id="address" name = "address" placeholder = "Address" required>
-                    </div>
-                    <div class="form-group col-md-2">
-                    <label for="postalCode">Postal Code</label>
-                    <input type="text" class="form-control" id="postalCode" name = "postalCode" placeholder = "Postal Code">
+                        <label for="dateOfInfection">Date of Infection</label>
+                        <input type="date" class="form-control" id="dateOfInfection" name = "dateOfInfection" placeholder="YYYY-MM-DD" required>
                     </div>
                 </div>
                 <button type="submit" value="Submit" name = "submit" class="btn btn-primary">Submit</button>
                 <?php
-                if (isset($success) && $success == true) {
-                    echo '<h3 style="color:green; text-align:center;font-family:Museosans;transition: color 1s ease-in 1s">Entry Added</h3>';
+                if (isset($success) && $success) {
+                    echo '<h3 style="color:green; text-align:center;font-family:Museosans;serif transition: color 1s ease-in 1s">Entry Added</h3>';
+                    echo "<script> location.href='".BASE_URL."Infection/displayInfection.php'; </script>";
+                    exit();
                 } else {
                     echo "";
                 }
                 ?>
-            </form>
+        </div>
+    </div>
+    <div>
+        <div>
+            <div id = "footer">
+                <?php include_once '../footer.php';?>
                 <div>
-            <div>
-          <div id = "footer">
-              <?php include_once '../footer.php';?>
-          <div>
-      <div>
+                    <div>
 </body>
 </html>
